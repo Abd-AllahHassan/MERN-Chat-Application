7900ab51ec9e92a3d3285e0f8f0993baa6de79a2
Abd-AllahHassan <aba252004@gmail.com>
2024-10-27 21:58:18 +0300

    Add vercel.json configuration

--- api/index.js
+++ api/index.js
@@ -118,7 +118,7 @@ app.post('/register', async (req,res) => {
   }
 });
 
-const server = app.listen(4040,() => {
+const server = app.listen(process.env.PORT,() => {
   console.log('Server is running on port 4040');
 });
 
